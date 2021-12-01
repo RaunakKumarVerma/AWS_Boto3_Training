76db930355226d8e31fb3900306ccc76c34e256a
RaunakKumarVerma <raunak.k.verma@gmail.com>
2021-12-01 14:13:51 +0530

    Get Account details

---
+++ 01.basics.py
@@ -0,0 +1,19 @@
+import boto3
+import json
+
+
+objClient = boto3.client('sts')
+
+response=objClient.get_caller_identity()
+
+userId = response['UserId']
+account=response['Account']
+arn=response['Arn']
+
+output={
+    "UserID":userId,
+    "Account":account,
+    "Arn": arn
+}
+
+print(json.dumps(output,indent=4))
